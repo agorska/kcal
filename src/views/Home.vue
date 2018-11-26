899c8e258272c05cca67f78c8647dfc0d0314fd9
Agnieszka <xagnieszka.gorskax@gmail.com>
2018-11-26 20:28:38 +0100

    prepare files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" id="searchFood" name="searchFood" class="home-search__input" 
+      v-model="searchFood"
+      @input="handleInput"
+      />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { debounce } from 'lodash.debounce';
+
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      searchFood: '',
+    };
+  },
+  methods: {
+    handleInput() {
+      console.log(this.searchFood);
+    },
   },
 };
 </script>
