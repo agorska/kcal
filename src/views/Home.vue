2f7020d1ad023db5d8b8144ba6dab717b3db0d00
Agnieszka <xagnieszka.gorskax@gmail.com>
2018-11-27 21:10:37 +0100

    podlaczenie api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="home">
-    <input type="text" id="searchFood" name="searchFood" class="home-search__input" 
-      v-model="searchFood"
-      @input="handleInput"
-      />
+    <input type="text" id="searchFood" name="searchFood" class="home-search__input" v-model="searchFood" @input="handleInput"/>
+    <ul>
+      <li v-for="item in results" :key="item.food_name">{{ item.food_name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { debounce } from 'lodash.debounce';
-
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 
 export default {
   name: 'home',
   data() {
     return {
       searchFood: '',
+      results: [],
     };
   },
   methods: {
-    handleInput() {
-      console.log(this.searchFood);
-    },
+    handleInput: debounce(function () {
+      axios({
+        method: 'get',
+        url: `https://trackapi.nutritionix.com/v2/search/instant?query=${this.searchFood}`,
+        headers: {
+          'x-app-id': '740969e3',
+          'x-app-key': '04d25b3db76aca93f78186a4987563e8',
+          'x-remote-user-id': '0',
+        },
+      }).then((response) => {
+        this.results = (response.data.common);
+      }).catch((error) => {
+        console.log(error);
+      });
+    }, 500),
   },
 };
 </script>
