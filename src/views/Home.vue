b8a306d11a39da81ef838ebbb9a6a072015b04ae
Agnieszka <xagnieszka.gorskax@gmail.com>
2018-12-14 19:30:21 +0100

    displayed raw nutrition object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="home">
-    <input type="text" id="searchFood" name="searchFood" class="home-search__input" v-model="searchFood" @input="handleInput"/>
+    <input
+      type="text"
+      id="searchFood"
+      name="searchFood"
+      class="home-search__input"
+      v-model="searchFood"
+      @input="handleInput"
+      placeholder="Write product name"
+    />
     <ul>
-      <li v-for="item in results" :key="item.food_name">{{ item.food_name }}</li>
+       <li v-for="item in results" :key="item.food_name">
+         <a href="#" v-on:click="nutrition">{{ item.food_name }}</a>
+        </li>
     </ul>
+    <div v-for="item in roman" :key="item.food_name"> {{ item }} </div>
   </div>
 </template>
 
@@ -17,6 +28,8 @@ export default {
     return {
       searchFood: '',
       results: [],
+      nutritions: '',
+      roman: [],
     };
   },
   methods: {
@@ -35,6 +48,26 @@ export default {
         console.log(error);
       });
     }, 500),
+    nutrition: function (el) {
+      this.nutritions = el.path[0].innerText.toString();
+      axios({
+        method: 'post',
+        url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+        headers: {
+          'x-app-id': '740969e3',
+          'x-app-key': '04d25b3db76aca93f78186a4987563e8',
+          'x-remote-user-id': '0',
+          'Content-Type': 'application/json',
+        },
+        data: {
+          query: this.nutritions,
+        },
+      }).then((response) => {
+        this.roman = (response.data.foods);
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
   },
 };
 </script>
