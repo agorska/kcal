8f19982cfe21331d50a44b2e40c6d612a7db14ad
Agnieszka <xagnieszka.gorskax@gmail.com>
2019-02-07 15:45:37 +0100

    resolved calculator problem

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,18 +1,20 @@
 <template>
   <section class="charts">
     <h2>Nutritions</h2>
-    <div class="details-data">
-      <div v-for="(value, key, index) in sumObjectsByKey(eatenDetailedList)" :key="value.id">
-        <div v-if="index >= 1">{{ key }} {{ value }}</div>
-      </div>
-    </div>
-    <bar-chart :data="[
-        ['Protein', (this.whoNorms.Protein)],
-        ['Fat', (this.whoNorms.Fat)],
-        ['Carbs', (this.whoNorms.Carbs)]
+    <div class="details-data">{{ chartData }}</div>
+
+    <bar-chart 
+      height="200px"
+      :colors="['#666']"
+      :data="[
+        ['Norm Protein', (this.whoNorms.Protein),],
+        ['Protein', (this.chartData.Protein)],
+        ['Norm Fat', (this.whoNorms.Fat)],
+        ['Fat', (this.chartData.Fat)],
+        ['Norm Carbs', (this.whoNorms.Carbs)],
+        ['Carbs', (this.chartData.Carbs)],
       ]"
       ></bar-chart>
-    <pie-chart :donut="true" :data="[['Fat', normForPieChart((this.whoNorms.Fat), 10)], ['Strawberry', 23]]"></pie-chart>
   </section>
 </template>
 
@@ -22,7 +24,6 @@
     text-align center
 </style>
 
-
 <script>
 import { EventBus } from './event-bus';
 
@@ -30,11 +31,11 @@ export default {
   name: 'charts',
   data() {
     return {
-      eatenDetailedList: '',
+      eatenDetailedList: [],
       chartData: '',
       num: 3,
       whoNorms: {
-        Protein: 60,
+        Protein: 80,
         Fat: 80,
         Carbs: 340,
         Water: 2500,
@@ -57,22 +58,26 @@ export default {
   mounted() {
     EventBus.$on('toCalc', (eatenDetailedList) => {
       this.eatenDetailedList = eatenDetailedList;
+      this.sumObjectsByKey();
+    });
+    EventBus.$on('updateCharts', (n) => {
+      (this.eatenDetailedList).splice(n, 1);
+      this.sumObjectsByKey();
     });
   },
   methods: {
-    sumObjectsByKey(objs) {
-      return objs.reduce((a, b) => {
-        for (const k in b) {
-          if (b.hasOwnProperty(k)) {
-            a[k] = (a[k] || 0) + b[k];
-          }
-        }
-        this.chartData = a;
-        return a;
-      }, {});
-    },
-    normForPieChart(a, b) {
-      return a - b;
+    sumObjectsByKey() {
+      const myData = this.eatenDetailedList;
+      const keys = (Object.keys(this.whoNorms));
+      
+      const result = myData.reduce((r, e, i, a) => {
+        keys.forEach(k => r[k] = (r[k] || 0) + parseInt(e[k]));
+          if(!a[i + 1]) Object.keys(e)
+            .filter(k => typeof e[k] == 'string')
+            .forEach(k => r[k] /= myData.length)
+            return r;
+        }, {})
+      this.chartData = result;
     },
   },
 };
